24d3e06197f94f77bac0bed7eb079c0de77bc2ed
devin-weybee <devin@weybee.in>
2025-08-22 09:57:09 +0530

    make changes in code logic

--- codingChallenge1.js
+++ codingChallenge1.js
@@ -2,35 +2,37 @@ let weightMark, weightJohn, heightMark, heightJohn;
 //bmi = mass/(height**2);
 
 //TEST Data 1
-console.log("TEST DATA 1")
+console.log("TEST DATA 1");
 
-weightMark = 78
-heightMark = 1.69
+weightMark = 78;
+heightMark = 1.69;
 
-let bmiMark = weightMark/(heightMark**2);
-console.log("BMI MARK : ",bmiMark.toFixed(2))
+let bmiMark = weightMark / heightMark ** 2;
+console.log("BMI MARK : ", bmiMark.toFixed(2));
 
-weightJohn = 92
-heightJohn = 1.95
+weightJohn = 92;
+heightJohn = 1.95;
 
-let bmiJohn = weightJohn/(heightJohn**2);
-console.log("BMI John : ",bmiJohn.toFixed(2))
+let bmiJohn = weightJohn / heightJohn ** 2;
+console.log("BMI John : ", bmiJohn.toFixed(2));
 
-let markHigherBMI = bmiMark > bmiJohn ? true : false;console.log("Mark has higher BMI than John : ",markHigherBMI)
+let markHigherBMI = bmiMark > bmiJohn;
+console.log("Mark has higher BMI than John : ", markHigherBMI);
 
 //TEST Data 2
-console.log("TEST DATA 2")
+console.log("TEST DATA 2");
 
-weightMark = 95
-heightMark = 1.88
+weightMark = 95;
+heightMark = 1.88;
 
-bmiMark = weightMark/(heightMark**2);
-console.log("BMI MARK : ",bmiMark.toFixed(2))
+bmiMark = weightMark / heightMark ** 2;
+console.log("BMI MARK : ", bmiMark.toFixed(2));
 
-weightJohn = 85
-heightJohn = 1.76
+weightJohn = 85;
+heightJohn = 1.76;
 
-bmiJohn = weightJohn/(heightJohn**2);
-console.log("BMI John : ",bmiJohn.toFixed(2))
+bmiJohn = weightJohn / heightJohn ** 2;
+console.log("BMI John : ", bmiJohn.toFixed(2));
 
-markHigherBMI = bmiMark > bmiJohn ? true : false;console.log("Mark has higher BMI than John : ",markHigherBMI)
+markHigherBMI = bmiMark > bmiJohn;
+console.log("Mark has higher BMI than John : ", markHigherBMI);
